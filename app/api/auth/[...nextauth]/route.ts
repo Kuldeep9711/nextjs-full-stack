ea2cf835a2d4530db6da14362f7ba3d55a123fe2
Kuldeep Saini <k72125642@gmail.com>
2025-11-01 23:51:13 +0530

    add: credential function

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import { AuthOptions } from "@/lib/auth";
+import { authOptions } from "@/lib/auth";
 
 import NextAuth from "next-auth";
 
-const handler = NextAuth(AuthOptions)
+const handler = NextAuth(authOptions)
 
 export {handler as GET, handler as POST};
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,49 @@
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
 
-export const AuthOptions: NextAuthOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
-        }),
-        // ...add more providers here
-    ],
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "passsword" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or passsword");
+        }
+
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("No user found with this");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.passsword
+          );
+
+          if (!isValid) {
+            throw new Error("invalid password");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          console.error("Auth error: ", error);
+          throw error;
+        }
+      },
+    }),
+]
+
 };
