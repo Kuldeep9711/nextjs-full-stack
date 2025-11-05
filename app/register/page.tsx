9a1812931f70f45fbedee96f4e3cea9b47120f4d
Kuldeep Saini <k72125642@gmail.com>
2025-11-05 21:39:42 +0530

    Added login button

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,14 +9,68 @@ function RegisterPage() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const router = useRouter();
 
-  const handleSumit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSumit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       alert("password do not match");
       return;
     }
+
+    try {
+      // react -query
+      // loading, error, debounce
+        const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+         }),
+        })
+
+        const data = await res.json();
+
+        if(!res.ok) {
+          throw new Error(data.error || "Registration failed" );
+        }
+
+        console.log(data);
+        router.push("/login");
+
+    } catch(error) {
+   console.error(error);
+    }
   };
-  return <div>RegisterPage</div>;
+  return <div>
+    <h1>Register</h1>
+    <form onSubmit={handleSumit}>
+      <input
+      type="email"
+      placeholder="Email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+      type="password"
+      placeholder="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      />
+      <input 
+      type="password"
+      placeholder="Confirm Password"
+      value={confirmPassword}
+      onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <button type="submit">Register</button>
+    </form>
+    <div>
+      <p>Already have an account? <a href="/login">Login</a>
+      </p>
+    </div>
+  </div>;
 }
 
 export default RegisterPage;
